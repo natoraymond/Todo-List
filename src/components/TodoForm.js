9618110e692869701c6a615977972401070cadb3
Natoraymod <nnamdi.raymondoct@gmail.com>
2023-07-11 01:56:13 -0700

    first commit

---
+++ src/components/TodoForm.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect, useRef } from 'react'
+
+function TodoForm(props) {
+    const [input, setInput] = useState(props.edit ? props.edit.value : '');
+
+    const inputRef = useRef(null)
+
+    useEffect(() => {
+        inputRef.current.focus()
+    })
+
+
+    const handlChange = e => {
+        setInput(e.target.value);
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        props.onSubmit ({  
+            id: Math.floor(Math.random() *10000), 
+            text: input
+        });
+        
+        setInput('');
+    };
+  return (
+    <form className='todo-form' onSubmit={handleSubmit}>
+        {props.edit ? (
+            <>
+              <input type='text' placeholder='Update Your Items' valu={input} name='text' className='todo-input edit'
+              onChange={handlChange}
+              ref={inputRef} 
+              />
+              <button className='todo-button edit'>Update</button>
+              </>
+              ) :
+              (
+                <>
+                <input type='text' placeholder='Add a todo' valu={input} name='text' className='todo-input'
+                onChange={handlChange}
+                ref={inputRef} 
+                />
+                <button className='todo-button'>Add todo</button>
+                </>
+              )
+            
+            }
+
+      
+    </form>
+  );
+}
+
+export default TodoForm;
